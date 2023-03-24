51840be1b0b0e887049d934ed79be3ac428d28a1
dinar <dina.rotenberg483@gmail.com>
2023-03-25 01:07:36 +0300

    Initial commit

---
+++ src/main/java/tasks/java/App.java
@@ -0,0 +1,13 @@
+package tasks.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
