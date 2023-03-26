fd4d2d297be792a6201bfd7de9d57bfcfad313ce
dinar <dina.rotenberg483@gmail.com>
2023-03-27 00:27:04 +0300

    EndlessCycle

---
+++ src/main/java/Interview/ExecutableComment.java
@@ -0,0 +1,9 @@
+package Interview;
+
+public class ExecutableComment {
+    public static void main(String[] args) {
+        // the line below this gives an output
+        // \u000d
+        System.out.println("comment executed");
+    }
+}
