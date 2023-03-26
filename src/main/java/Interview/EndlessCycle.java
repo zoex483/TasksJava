30c3943e2464594a989985f81bdee18eaf2981a8
dinar <dina.rotenberg483@gmail.com>
2023-03-27 00:21:54 +0300

    EndlessCycle

---
+++ src/main/java/Interview/EndlessCycle.java
@@ -0,0 +1,11 @@
+package Interview;
+
+public class EndlessCycle {
+    public static void main(String[] args) {
+        int start = Integer.MAX_VALUE - 1;
+        for (int i = start; i <= start + 1; i++) {
+            // бесконечный цикл
+            System.out.println(i); // бесконечность цикла
+        }
+    }
+}
